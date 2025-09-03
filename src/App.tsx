3abc4fda673a9ef3a26639b4955e14f1f35a4e67
Maximilian <scientificresearch309@gmail.com>
2025-09-03 12:40:56 +0200

    German nouns are there using axios

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,25 @@
 // import './App.css';
+import { useEffect } from 'react';
+import axios from 'axios';
+
+const germanNounsURL =
+  'https://edwardtanguay.vercel.app/share/germanNouns.json';
 
 function App() {
+  useEffect(() => {
+    (async () => {
+      const response = await axios(germanNounsURL);
+      const germanNouns = response.data;
+      console.log(germanNouns);
+    })();
+  }, []);
+
   return (
     <div className="App">
       <h1>useEffect-germanNouns-React-Vite-Typescript</h1>
-      <p>Welcome to this Site.</p>
+      <h3>Welcome to this Site.</h3>
+
+      <p>There are {} germnan Nouns!</p>
     </div>
   );
 }
