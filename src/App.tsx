f8e632e7fe8d65848c85bba83a73e384d2739926
Maximilian <scientificresearch309@gmail.com>
2025-09-03 13:16:34 +0200

    showing all german nouns and trying to remove Typescript errors

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,19 @@
 // import './App.css';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import axios from 'axios';
 
 const germanNounsURL =
   'https://edwardtanguay.vercel.app/share/germanNouns.json';
 
 function App() {
+  const [showGermanNouns, setShowGermanNouns] = useState([]);
+
   useEffect(() => {
     (async () => {
       const response = await axios(germanNounsURL);
       const germanNouns = response.data;
       console.log(germanNouns);
+      setShowGermanNouns(germanNouns);
     })();
   }, []);
 
@@ -19,7 +22,21 @@ function App() {
       <h1>useEffect-germanNouns-React-Vite-Typescript</h1>
       <h3>Welcome to this Site.</h3>
 
-      <p>There are {} germnan Nouns!</p>
+      <p>
+        There are <span>{showGermanNouns.length}</span> germnan Nouns in this
+        JSON file!
+      </p>
+
+      <div className="germanNouns">
+        {showGermanNouns.map((gn) => (
+          <div className="germanNoun">
+            <p>
+              {gn.article} {gn.singular}
+            </p>
+            <p>{gn.plural}</p>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
